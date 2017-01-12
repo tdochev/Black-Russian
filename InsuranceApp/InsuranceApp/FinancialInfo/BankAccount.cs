91b59771d5f17b3089ae07c2a6e4411ec51daa0b
Georgi Markov <markovood85@gmail.com>
2017-01-12 22:16:53 +0200

    fixes in  BankAccount.cs + custom exception added
    -removed inheritance of Root class which doesn't exist any more
    -added extra properties for creation and expiration time
    -changed owner's type to Person and made most properties read only
    -added extra constructor and a field for the non auto property IBAN
    -added private method for validating the IBAN

--- InsuranceApp/InsuranceApp/FinancialInfo/BankAccount.cs
+++ InsuranceApp/InsuranceApp/FinancialInfo/BankAccount.cs
@@ -1,28 +1,89 @@
 ﻿namespace InsuranceApp.FinancialInfo
 {
     using System;
-    using InsuranceApp.InsuranceObject;
-    class BankAccount:Root
+    using System.Text;
+    using Exceptions;
+    using Objects;
+
+    internal class BankAccount
     {
         //Manages bank account information
         //Consists information about bank, account owner, account IBAN
-        public HumanEntity AccountOwner { get; set; }
-        public Bank BankOrg { get; set; }
+        private const int initialExpirationTime = 10; // years
+        private string iban;
+
+        public DateTime CreationDate { get; }
+        public DateTime ExpirationDate { get; set; }
+        public Person AccountOwner { get; }
+        public Bank BankOrg { get; }
+
         public string IBAN
         {
             get
             {
-                return this.IBAN;
+                return this.iban;
             }
             set
             {
+                if (!ValidateIban(value))
+                {
+                    throw new InvalidIbanValidationExeption("Incorrectly entered IBAN: possible reason not allowed char/s");
+                }
 
+                this.iban = value.ToUpper();
             }
         }
-        public BankAccount(HumanEntity owner, Bank bank, string iban, DateTime start, DateTime end)
+
+        public BankAccount(Person owner, Bank bank, string iban)
         {
-            //TODO: Add code here
+            this.AccountOwner = owner;
+            this.BankOrg = bank;
+            this.IBAN = iban;
+            this.CreationDate = DateTime.Now;
+            this.ExpirationDate = this.CreationDate.AddYears(initialExpirationTime);
         }
 
+        public BankAccount(Person owner, Bank bank, string iban, DateTime creationDate, DateTime expirationDate)
+        {
+            this.AccountOwner = owner;
+            this.BankOrg = bank;
+            this.IBAN = iban;
+            this.CreationDate = creationDate;
+            this.ExpirationDate = expirationDate;
+        }
+
+        private static bool ValidateIban(string iban)
+        {
+            iban = iban.ToUpper(); //IN ORDER TO COPE WITH THE REGEX BELOW
+            if (String.IsNullOrEmpty(iban))
+                return false;
+            else if (System.Text.RegularExpressions.Regex.IsMatch(iban, "^[A-Z0-9]"))
+            {
+                iban = iban.Replace(" ", String.Empty);
+                string bank =
+                iban.Substring(4, iban.Length - 4) + iban.Substring(0, 4);
+                int asciiShift = 55;
+                StringBuilder sb = new StringBuilder();
+                foreach (char c in bank)
+                {
+                    int v;
+                    if (Char.IsLetter(c)) v = c - asciiShift;
+                    else v = int.Parse(c.ToString());
+                    sb.Append(v);
+                }
+                string checkSumString = sb.ToString();
+                int checksum = int.Parse(checkSumString.Substring(0, 1));
+                for (int i = 1; i < checkSumString.Length; i++)
+                {
+                    int v = int.Parse(checkSumString.Substring(i, 1));
+                    checksum *= 10;
+                    checksum += v;
+                    checksum %= 97;
+                }
+                return checksum == 1;
+            }
+            else
+                return false;
+        }
     }
 }
