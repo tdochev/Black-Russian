f52076b894ac693582f806e9ef512eef1ebe2e2a
marvasgit <marin.vassilev@gmail.com>
2017-01-12 23:42:55 +0200

    Added Verification Class
    Class addeed, Person class refactored

---
+++ InsuranceApp/InsuranceApp/Engine/Verification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InsuranceApp.Engine
+{
+
+    public class Verification
+    {
+        internal static void String(string value,string Name)
+        {
+           if (string.IsNullOrEmpty(value))
+                {
+                    // TODO: Warn the user that value cannot be left empty
+                    throw new NotImplementedException();
+                }
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    // TODO: Warn the user that value cannot be Whitespace
+                    throw new NotImplementedException();
+                }
+        }
+
+        internal static void Phone(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
