98ddad42109da98a61b6dc5433a2d9d32d2be0dc
Georgi Markov <markovood85@gmail.com>
2017-01-12 15:44:28 +0200

    fixed validation
    -added field for the non auto property, since without it, the result is
    "stack overflow exeption"
    -removed validation for "STS"(What is STS?!)  and replaced with
    validation for digits since BIC code does not contain digits
    -removed constructor with arguments because it gives error --> "The
    'this' object cannot be used before all of its fields are assigned to".
    The error ocures due to the unassigned field, which is needed due to the
    fact we need to make validations and cannot be assigned initially
    because structs cannot have field initializers...

--- InsuranceApp/InsuranceApp/FinancialInfo/Bank.cs
+++ InsuranceApp/InsuranceApp/FinancialInfo/Bank.cs
@@ -5,32 +5,31 @@
     public struct Bank 
     {
         //Consists information about bank - name, code
+        private string bic;
         public string BankName { get; set; }
 
         public string BICCode
         {
             get
             {
-                return this.BICCode;
+                return this.bic;
             }
             set
             {
                 if (value.Length!=8)
                 {
-                    throw new ArgumentException("Invalid BIC code: Code length less than 8 symbols");
+                    throw new ArgumentException("Invalid BIC/SWIFT code: Code length less than 8 symbols");
                 }
-                if (value.ToUpper().Substring(0, 3) != "STS")
-                {
-                    throw new ArgumentException("Invalid BIC code: BIC code must start with STS");
-                }
-                BICCode = value.ToUpper();
-            }
-        }
 
-        public Bank(string bank, string bicCode)
-        {
-            this.BankName = bank;
-            this.BICCode = bicCode;
+                if (value.Contains("0") || value.Contains("1") || value.Contains("2") || value.Contains("3") ||
+                    value.Contains("4") || value.Contains("5") || value.Contains("6") || value.Contains("7") ||
+                    value.Contains("8") || value.Contains("9"))
+                {
+                    throw new ArgumentException("Invalid BIC/SWIFT code: Code cannot contain digits!");
+                }
+
+                bic = value.ToUpper();
+            }
         }
     }
 }
